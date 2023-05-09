da91f87b4ef81dc6550bfbee3ef2775a378a0709
riya.sankesara <riya.sankesra@ics-global.in>
2023-05-09 16:20:10 +0530

    first commit

---
+++ CoreApi/Controller/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using CoreApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        IList<Employee> employees = new List<Employee>()
+        {
+            new Employee()
+                {
+                    EmployeeId = 1, EmployeeName = "Riya Sankesara", Address = "Kalol", Department = "IT"
+                },
+                new Employee()
+                {
+                    EmployeeId = 2, EmployeeName = "Rashmin Mevada", Address = "Visnagar", Department = "HR"
+                },
+                new Employee()
+                {
+                    EmployeeId = 3, EmployeeName = "Dhaval Mistry", Address = "Ahmedabad", Department = "IT"
+                },
+                new Employee()
+                {
+                    EmployeeId = 4, EmployeeName = "Disha Tamboli", Address = "Himmatnagar", Department = "Sales"
+                },
+                new Employee()
+                {
+                    EmployeeId = 5, EmployeeName = "Vibha Shah", Address = "Rajsthan", Department = "HR"
+                },
+        };
+        [HttpGet]
+        [Route("GetAllEmployees")]
+        public IList<Employee> GetAllEmployees()
+        {
+            //Return list of all employees
+            return employees;
+        }
+        [HttpGet]
+        [Route("GetEmployeeDetails")]
+        public Employee GetEmployeeDetails(int id)
+        {
+            //Return a single employee detail
+            var employee = employees.FirstOrDefault(e => e.EmployeeId == id);
+            if (employee == null)
+            {
+                return new Employee();
+            }
+            return employee;
+        }
+
+    }
+}
---
+++ CoreApi/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+
+        }
+        [HttpGet]
+        public List<Users> GetUser()
+        {
+            List<Users> _users = new List<Users>()
+            {
+                new Users()
+                {
+                    Name = "Riya",
+                    Email = "Riya@gmail.com",
+                    Password = "riya123"
+                }
+            };
+            return _users;
+        }
+
+    }
+    public class Users
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
